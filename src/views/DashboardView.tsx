6a54534410a0f18df3c20c0e6325419b64de50bf
Jordan Umusu <jordanumusu@gmail.com>
2021-10-24 07:48:01 -0400

    axios

--- src/views/DashboardView.tsx
+++ src/views/DashboardView.tsx
@@ -3,7 +3,7 @@ import Layout from "../components/Layout";
 import { ArrowUpIcon, ArrowDownIcon } from "@heroicons/react/outline";
 import { parseISO, format, subDays } from "date-fns";
 import { InformationCircleIcon } from "@heroicons/react/solid";
-import { Dialog,Transition } from "@headlessui/react";
+import { Dialog, Transition } from "@headlessui/react";
 import {
   ResponsiveContainer,
   PieChart,
@@ -27,7 +27,7 @@ function DashboardView() {
     payload?: any;
     label?: any;
   }
-  const [isOpen, setIsOpen] = useState(true);
+  const [isOpen, setIsOpen] = useState(false);
   const expenditureData = [
     { name: "Rent & Utilities", value: 5 },
     { name: "Food Costs", value: 40 },
@@ -102,6 +102,16 @@ function DashboardView() {
     setIsOpen(true);
   }
 
+  let prediction : any= ""
+  useEffect(() => {
+    try{
+     axios.post("http://127.0.0.1:5000/").then((response)=> prediction = response.data);
+    }
+    catch (e) {
+      console.log(e)
+    }
+  }, []
+  );
   return (
     <Layout>
       <div className="flex-col pr-10 pl-6 grid-rows-12">
@@ -142,75 +152,77 @@ function DashboardView() {
         <div className="flex">
           <div className="bg-white flex flex-col shadow-lg w-3/4">
             <div className="flex border-b-2 px-8 py-4 w-full justify-between">
-              <h2 className=" text-xl font-semibold ">
-                Revenue Overview
+              <div className="flex">
+                <h2 className=" text-xl font-semibold">Revenue Overview</h2>
+
                 <button onClick={openModal}>
-                  <InformationCircleIcon className="h-4 w-4 text-blue-800" />
+                  <InformationCircleIcon className="h-6 w-6 mx-3 text-blue-400 animate-pulse" />
                 </button>
                 <Transition appear show={isOpen} as={Fragment}>
-        <Dialog
-          as="div"
-          className="fixed inset-0 z-10 overflow-y-auto"
-          onClose={closeModal}
-        >
-          <div className="min-h-screen px-4 text-center">
-            <Transition.Child
-              as={Fragment}
-              enter="ease-out duration-300"
-              enterFrom="opacity-0"
-              enterTo="opacity-100"
-              leave="ease-in duration-200"
-              leaveFrom="opacity-100"
-              leaveTo="opacity-0"
-            >
-              <Dialog.Overlay className="fixed inset-0" />
-            </Transition.Child>
-
-            {/* This element is to trick the browser into centering the modal contents. */}
-            <span
-              className="inline-block h-screen align-middle"
-              aria-hidden="true"
-            >
-              &#8203;
-            </span>
-            <Transition.Child
-              as={Fragment}
-              enter="ease-out duration-300"
-              enterFrom="opacity-0 scale-95"
-              enterTo="opacity-100 scale-100"
-              leave="ease-in duration-200"
-              leaveFrom="opacity-100 scale-100"
-              leaveTo="opacity-0 scale-95"
-            >
-              <div className="inline-block w-full max-w-md p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl">
-                <Dialog.Title
-                  as="h3"
-                  className="text-lg font-medium leading-6 text-gray-900"
-                >
-                  Payment successful
-                </Dialog.Title>
-                <div className="mt-2">
-                  <p className="text-sm text-gray-500">
-                    Your payment has been successfully submitted. We’ve sent you
-                    an email with all of the details of your order.
-                  </p>
-                </div>
-
-                <div className="mt-4">
-                  <button
-                    type="button"
-                    className="inline-flex justify-center px-4 py-2 text-sm font-medium text-blue-900 bg-blue-100 border border-transparent rounded-md hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500"
-                    onClick={closeModal}
+                  <Dialog
+                    as="div"
+                    className="fixed inset-0 z-10 overflow-y-auto"
+                    onClose={closeModal}
                   >
-                    Got it, thanks!
-                  </button>
-                </div>
+                    <div className="min-h-screen px-4 text-center">
+                      <Transition.Child
+                        as={Fragment}
+                        enter="ease-out duration-300"
+                        enterFrom="opacity-0"
+                        enterTo="opacity-100"
+                        leave="ease-in duration-200"
+                        leaveFrom="opacity-100"
+                        leaveTo="opacity-0"
+                      >
+                        <Dialog.Overlay className="fixed inset-0" />
+                      </Transition.Child>
+
+                      {/* This element is to trick the browser into centering the modal contents. */}
+                      <span
+                        className="inline-block h-screen align-middle"
+                        aria-hidden="true"
+                      >
+                        &#8203;
+                      </span>
+                      <Transition.Child
+                        as={Fragment}
+                        enter="ease-out duration-300"
+                        enterFrom="opacity-0 scale-95"
+                        enterTo="opacity-100 scale-100"
+                        leave="ease-in duration-200"
+                        leaveFrom="opacity-100 scale-100"
+                        leaveTo="opacity-0 scale-95"
+                      >
+                        <div className="inline-block w-full max-w-md p-6 my-8 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl">
+                          <Dialog.Title
+                            as="h3"
+                            className="text-lg font-medium leading-6 text-gray-900"
+                          >
+                            Payment successful
+                          </Dialog.Title>
+                          <div className="mt-2">
+                            <p className="text-sm text-gray-500">
+                              Your payment has been successfully submitted.
+                              We’ve sent you an email with all of the details of
+                              your order.
+                            </p>
+                          </div>
+
+                          <div className="mt-4">
+                            <button
+                              type="button"
+                              className="inline-flex justify-center px-4 py-2 text-sm font-medium text-blue-900 bg-blue-100 border border-transparent rounded-md hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-500"
+                              onClick={closeModal}
+                            >
+                              Got it, thanks!
+                            </button>
+                          </div>
+                        </div>
+                      </Transition.Child>
+                    </div>
+                  </Dialog>
+                </Transition>
               </div>
-            </Transition.Child>
-          </div>
-        </Dialog>
-      </Transition>
-              </h2>
               <div className="flex items-center space-x-2 ">
                 <ArrowDownIcon className="h-4 w-4 font-semibold text-red-500 transform" />
                 <p className="text-red-500  font-semibold">2.19%</p>
